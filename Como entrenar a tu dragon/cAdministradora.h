ffdba347698f64dcffe403ea96448ba37ba382e5
EmmiFiorini <143649576+EmmiFiorini@users.noreply.github.com>
2024-06-03 15:43:39 -0300

    empezamos con git ahora si!!

---
+++ Como entrenar a tu dragon/cAdministradora.h
@@ -0,0 +1,17 @@
+
+#pragma once
+#include "cVikingo.h"
+#include "cJinete.h"
+
+#include <list> 
+class cAdministradora {
+public:
+	//agrego una lista de cjinete, una lista de cvikingo, una lista de cdragon
+	list <cVikingo> listaDeVikingos;
+	list <cJinete> listaDeJinetes;
+	list <cDragon> listaDeDragones;
+
+	//void cVikingo::atacarDragones(cDragon* dragon, cVikingo* vikingo) {
+	//vikingo->get_habilidades()->combate(dragon->get_habilidades(), vikingo->get_habilidades()); //no sabemos por qué nos marca error!!!
+	void atacarDragon();
+};
---
+++ Como entrenar a tu dragon/cHabilidades.cpp
@@ -0,0 +1,1 @@
+#include "cHabilidades.h"
